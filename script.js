1060582b0928bc0a74e388d187213208d675dda9
Ishimwe Gad <135014965+ishgad@users.noreply.github.com>
2023-12-27 20:49:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("yesButton").addEventListener("click", function() {
+    window.location.href = "login.html";
+});
+
+document.getElementById("noButton").addEventListener("click", function() {
+    window.location.href = "signup.html";
+});
